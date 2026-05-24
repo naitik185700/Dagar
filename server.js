575bd543e1b6914d332436d7f77905a1406ba498
Vikram Singh <Vickyjangra7891@gmail.com>
2026-05-24 13:07:32 +0530

    refactor: update catch-all route to use regex pattern for better path matching

--- server.js
+++ server.js
@@ -224,7 +224,7 @@ const distPath = path.join(__dirname, 'dist');
 app.use(express.static(distPath));
 
 // Catch-all route for client-side routing
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 
